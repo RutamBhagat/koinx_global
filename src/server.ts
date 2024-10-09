9e216cec3543f1563faa33e43fd430368048e6f7
RutamBhagat <rutambhagat@gmail.com>
2024-10-09 15:04:03 -0700

    feat(server): initialize express server, setup cors, add swagger docs, add base endpoints and implement rate limiting

---
+++ src/server.ts
@@ -0,0 +1,58 @@
+import process from "node:process";
+import consola from "consola";
+import cors from "cors";
+import express from "express";
+import rateLimit from "express-rate-limit";
+import { mw as requestIp } from "request-ip";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+
+import "./utils/env";
+import "./jobs/crypto-job";
+
+const { PORT } = process.env;
+
+const app = express();
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(requestIp());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    handler: (req, res) => {
+      consola.warn(`DDoS Attempt from ${req.ip}`);
+      res.status(429).json({
+        error: "Too many requests in a short time. Please try in a minute.",
+      });
+    },
+  })
+);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/", (_req, res) => {
+  res.json({
+    message: "Welcome to the API!",
+  });
+});
+
+app.get("/healthcheck", (_req, res) => {
+  res.json({
+    message: "Server is running",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
+
+app.listen(PORT, () => {
+  consola.info(`Server running at http://localhost:${PORT}`);
+});
